96b9bd0a1c9ebd7e268cffee474cd5eac05caf56
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-16 23:51:49 -0700

    Create reee.py

---
+++ reee.py
@@ -0,0 +1,12 @@
+def Celsius_to_Kelvin(C): 
+    return (C + 273.15) 
+  
+# Driver Code 
+  
+# variable to hold the  
+# temperature in Celsius 
+C = 100
+  
+print("Temperature in Kelvin ( K ) = ",  
+                    Celsius_to_Kelvin(C)) 
+                      
